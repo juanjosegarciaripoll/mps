c635777b2ec15e0e98429f217841c9aedb2ae8cd
Juan Jose Garcia-Ripoll <jjgarcia@users.sf.net>
2014-10-06 12:36:32 +0200

    Document QuadraticForm

--- include/mps/qform.h
+++ include/mps/qform.h
@@ -27,22 +27,49 @@
 
 namespace mps {
 
+  /** Internal representation of a MPO as a quadratic form.
+      This object works with a scalar product $$\langle\psi|O|\phi\rangle$$,
+      where $O$ is a MPO, and the bra and ked, $\psi$ and $\phi$, are two
+      MPS. This scalar product, from the point of view of the k-th site, it
+      may be seen as a quadratic form $w^{T} \bar{O} v$, where $w$ and $v$
+      are the elements of the tensors $\psi$[k] and $\phi$[k] of the MPS
+      on that given site.
+  */
   template<class MPO>
   class QuadraticForm {
   public:
     typedef typename MPO::MPS MPS;
     typedef typename MPS::elt_t elt_t;
 
+    /** Initialize with the given MPO and bra and ket states. This function
+	assumes that we are inspecting site 'start', which may be at the
+	beginning or the end of the chain.*/
     QuadraticForm(const MPO &mpo, const MPS &bra, const MPS &ket, int start = 0);
+    /** Update the form changing the tensors of the bra and ket states. The
+	function updates the $\psi$ and $\phi$ states in $\langle\psi|O|\phi\rangle$
+	changing the tensor of those states at the site here(), and moving to
+	the next site, here()+1.*/
     void propagate_right(const elt_t &braP, const elt_t &ketP);
+    /** Update the form changing the tensors of the bra and ket states. The
+	function updates the $\psi$ and $\phi$ states in $\langle\psi|O|\phi\rangle$
+	changing the tensor of those states at the site here(), and moving to
+	the next site, here()-1.*/
     void propagate_left(const elt_t &braP, const elt_t &ketP);
+    /** The site at which the quadratic form is defined. */
     int here() const { return current_site_; }
+    /** Left bond dimension of the MPO at the present site. */
     index left_dimension(index site) const { return bond_dimensions_[site]; }
+    /** Right bond dimension of the MPO at the present site. */
     index right_dimension(index site) const { return bond_dimensions_[site+1]; }
+    /** Number of sites in the lattice. */
     index size() const { return left_matrix_.size(); }
 
+    /** Matrix representation of the quadratic form with respect to site here().*/
     const elt_t single_site_matrix() const;
+    /** Matrix representation of the quadratic form with respect to
+	sites here() and here()+1.*/
     const elt_t two_site_matrix() const;
+
   private:
 
     struct Pair {
