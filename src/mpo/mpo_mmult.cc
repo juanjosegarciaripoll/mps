8d3490e497dd85b3cb94e2d3e71e0f2286f512eb
Juan Jose Garcia-Ripoll <jjgarcia@users.sf.net>
2014-10-14 22:34:20 +0200

    Fixed mpo_mmult

--- src/mpo/mpo_mmult.cc
+++ src/mpo/mpo_mmult.cc
@@ -27,19 +27,20 @@ namespace mps {
     typedef typename MPO::elt_t Tensor;
     assert(A.size() == B.size());
 
-    index a1, i,a2,c1,k,c2;
     index L = A.size();
     MPO C = A;
 
-    for (index i = 0; i < L; i++) {
-      const Tensor &tA = A[i]; /* tA(a1,i,j,a2) */
-      const Tensor &tB = B[i]; /* tB(c1,j,k,c2) */
+    for (index n = 0; n < L; n++) {
+      const Tensor &tA = A[n]; /* tA(a1,i,j,a2) */
+      const Tensor &tB = B[n]; /* tB(c1,j,k,c2) */
       
       /* tC(a1,i,c1,k,c2,a2) = tB(c1,j,k,c2) tA(a1,i,j,a2) */
       Tensor tC = foldin(tB, 1, tA, 2);
+      index a1,i,a2,c1,k,c2;
       tC.get_dimensions(&a1, &i, &c1, &k, &c2, &a2);
+      tC = reshape(permute(permute(tC, 1, 2), 4, 5), a1*c1, i, k, a2*c2);
 
-      C.at(i) = reshape(permute(permute(tC, 1, 2), 4, 5), a1*c1, i, k, a2*c2);
+      C.at(n) = tC;
     }
     return C;
   }
