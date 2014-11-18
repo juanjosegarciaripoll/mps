1d5793b904a828818481b3c4cbe994c27a91cde4
Juanjo Garcia-Ripoll <jjgarcia@users.sourceforge.net>
2014-11-18 11:41:12 +0100

    The 'canonical' form of expected12 now starts always from the 0 site.

--- src/iTEBD/expected/expected.hpp
+++ src/iTEBD/expected/expected.hpp
@@ -29,16 +29,27 @@ namespace mps {
     if (!psi.is_canonical()) {
       return do_expected12(psi.canonical_form(), Op12, site);
     } else {
-      tensor::index a, i, b, j, c;
-      const Tensor &AlA = psi.combined_matrix(site);
-      const Tensor &BlB = psi.combined_matrix(site+1);
+      tensor::index a, i, b, j;
+      const Tensor &AlA = psi.combined_matrix(0);
+      const Tensor &BlB = psi.combined_matrix(1);
       AlA.get_dimensions(&a, &i, &b);
-      BlB.get_dimensions(&b, &j, &c);
-      Tensor AlABlB = reshape(fold(AlA, -1, BlB, 0), a, i*j, c);
-      Tensor v = psi.left_boundary(site);
-      typename Tensor::elt_t value = trace(propagate_right(v, AlABlB, Op12));
-      typename Tensor::elt_t norm = trace(propagate_right(v, AlABlB));
-      return value / real(norm);
+      BlB.get_dimensions(&b, &j, &a);
+      Tensor v1 = psi.left_boundary(0);
+      Tensor v2 = v1;
+      if (site & 1) {
+        v1 = propagate_right(v1, AlA);
+        v2 = v1;
+        const Tensor BlBAlA = reshape(fold(BlB, -1, AlA, 0), b, j*i, b);
+        v1 = propagate_right(v1, BlBAlA, Op12);
+        v2 = propagate_right(v2, BlBAlA);
+        v1 = propagate_right(v1, BlB);
+        v2 = propagate_right(v2, BlB);
+      } else {
+        const Tensor AlABlB = reshape(fold(AlA, -1, BlB, 0), a, i*j, a);
+        v1 = propagate_right(v1, AlABlB, Op12);
+        v2 = propagate_right(v2, AlABlB);
+      }
+      return trace(v1) / trace(v2);
     }
   }
 
