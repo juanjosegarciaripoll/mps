92a316323098c93adb23a51679f59ddce679257c
Juan Jose Garcia-Ripoll <jjgarcia@users.sf.net>
2014-12-28 14:51:15 +0100

    Removed MPS_DEBUG_ARNOLDI from tools.h

--- include/mps/tools.h
+++ include/mps/tools.h
@@ -29,8 +29,7 @@ namespace mps {
 
   enum {
     MPS_DEBUG_NONE = 0,
-    MPS_DEBUG_SIMPLIFY_MANY = 2,
-    MPS_DEBUG_ARNOLDI = 4
+    MPS_DEBUG_SIMPLIFY_MANY = 2
   };
   extern int debug_flags;
 
